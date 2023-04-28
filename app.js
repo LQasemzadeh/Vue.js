e2fb4266a693d6976a35d002e5fe67ae16aa74d6
lqasemzadeh <lqasemzadeh@gmail.com>
2023-04-28 18:44:49 +0330

    vue.js1

--- app.js
+++ app.js
@@ -5,14 +5,23 @@ const app = Vue.createApp({
             title: 'The Final Empire',
             author: 'Eric John',
             age: '35',
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e) {
+        handleEvent(e, data) {
             console.log('e, e.type')
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 
